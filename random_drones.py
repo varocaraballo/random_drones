72ce512e6e1050e56147f090bf706b2335976290
Luis Evaristo Caraballo de la Cruz <varo.caraballo87@gmail.com>
2018-04-02 18:27:52 +0200

    Upgrading simula_drones

--- random_drones.py
+++ random_drones.py
@@ -140,7 +140,21 @@ def create_gossiping_statistics(robots):
     return gossiping_statistics
 
 
-def simula_drones(g, init_pos_robot, bouncing=False, ni=None, edges_circles=None, p = None):
+def update_simulation_state(g, current_pos, robot, new_pos, new_pos_robot, covering_statistics, robot_circles, edges_circles):
+	covering_statistics[(current_pos,new_pos)] = 0
+
+	if robot_circles is not None:
+		robot_circles[robot] = edges_circles[(current_pos,new_pos)]
+
+	if new_pos in new_pos_robot:
+		new_pos_robot[new_pos] += [robot]
+	else:
+		new_pos_robot[new_pos] = [robot]
+
+
+
+def simula_drones(g, init_pos_robot, bouncing=False, ni=None, robot_circles=None, edges_circles=None, p = None):
+	# p is the probability to shift to a neighboring circle
     logging.basicConfig(format='%(message)s',filename='statistics.log',level=logging.DEBUG)
     c = 0
     current_pos_robot = init_pos_robot
@@ -149,7 +163,7 @@ def simula_drones(g, init_pos_robot, bouncing=False, ni=None, edges_circles=None
         p = 0.5
     weights = [1-p, p]
     cover_statistics = create_covering_statistics(g)
-    gsp_statistics = create_gossiping_statistics([x if type(x) is not tuple else x[0] for p in init_pos_robot for x in init_pos_robot[p]])
+    gsp_statistics = create_gossiping_statistics([x for p in init_pos_robot for x in init_pos_robot[p]])
 
     logging.info('Statictics format:')
 
@@ -179,60 +193,48 @@ def simula_drones(g, init_pos_robot, bouncing=False, ni=None, edges_circles=None
         for r in gsp_statistics:
             gsp_statistics[r][r] = c
 
-        new_pos_robot = {}      
+        new_pos_robot = {}  
+        
         for p in current_pos_robot:
             c_robots = current_pos_robot[p]
             if len(c_robots)>1:
                 for r1 in c_robots:
-                    robot1 = r1
-                    if type(r1) is tuple:
-                        robot1 = r1[0]
                     for r2 in c_robots:
                         if r2 == r1:
                             continue
-                        robot2 = r2
-                        if type(r2) is tuple:
-                            robot2 = r2[0]
                         for r in gsp_statistics:
-                            if gsp_statistics[robot1][r]<gsp_statistics[robot2][r]:
-                                gsp_statistics[robot1][r] = gsp_statistics[robot2][r]
+                            if gsp_statistics[r1][r]<gsp_statistics[r2][r]:
+                                gsp_statistics[r1][r] = gsp_statistics[r2][r]
 
 
             if not bouncing or len(c_robots)==1:
                 #pick randomly a destiny node for every robot in this position
                 for r in c_robots:
-                    if type(r) is tuple: #if r is a tuple then r = (robot, circle) where 'robot' is the label of the robot and 'circle' is the label of the circle where the robot is                       
+                    if robot_circles is not None: #if r is a tuple then r = (robot, circle) where 'robot' is the label of the robot and 'circle' is the label of the circle where the robot is                       
                         picked = 0;
                         if len(g[p])>1:
                             action = pick_element(choices, weights)
-                            picked = 0 if ((action == 'stay' and edges_circles[(p,g[p][0])] == r[1]) or (action == 'shift' and edges_circles[(p,g[p][0])] != r[1])) else 1
+                            picked = 0 if ((action == 'stay' and edges_circles[(p,g[p][0])] == robot_circles[r]) or (action == 'shift' and edges_circles[(p,g[p][0])] != robot_circles[r])) else 1
                     else:
                         picked  = random.randint(0,len(g[p])-1)
 
                     new_pos = g[p][picked]
-                    cover_statistics[(p,new_pos)] = 0
-                    new_r = r
-                    if type(r) is tuple:
-                        new_r = (r[0], edges_circles[(p,new_pos)])
-                    if new_pos in new_pos_robot:
-                        new_pos_robot[new_pos] += [new_r]
-                    else:
-                        new_pos_robot[new_pos] = [new_r]
+                    
+                    update_simulation_state(g, p, r, new_pos, new_pos_robot, cover_statistics, robot_circles, edges_circles)
             else:
                 #there are at most two robots per node and two possible destiny nodes per node, then, send a robot per node 
                 for i in [0,1]:
                     r = c_robots[i];
-                    if type(r) is tuple: #if r is a tuple then r = (robot, circle) where 'robot' is the label of the robot and 'circle' is the label of the circle where the robot is
-                        new_pos  = g[p][0] if edges_circles[(p, g[p][0])] == r[1] else g[p][1]
+                    if robot_circles is not None: #if r is a tuple then r = (robot, circle) where 'robot' is the label of the robot and 'circle' is the label of the circle where the robot is
+                        new_pos  = g[p][0] if edges_circles[(p, g[p][0])] == robot_circles[r] else g[p][1]
                     else:
                         new_pos = g[p][i]   
-                    cover_statistics[(p,new_pos)] = 0               
-                    if new_pos in new_pos_robot:
-                        new_pos_robot[new_pos] += [r]
-                    else:
-                        new_pos_robot[new_pos] = [r]
+
+                    update_simulation_state(g, p, r, new_pos, new_pos_robot, cover_statistics, robot_circles, edges_circles)
+                    
         c += 1
         current_pos_robot = new_pos_robot
+       
         logging.info('---------------------------')
         logging.info('iter %d',c)
         logging.info('** Uncovering info **')
@@ -252,10 +254,11 @@ def simula_drones(g, init_pos_robot, bouncing=False, ni=None, edges_circles=None
 
 
 # my_g = {1:[3],2:[1],3:[4],4:[2,6],5:[8],6:[9],7:[4,5],8:[10],9:[7,12],10:[7],11:[9],12:[13],13:[11]}
-# e_circles = {(1,3):'A',(3,4):'A',(4,2):'A',(2,1):'A',(4,6):'B',(6,9):'B',(9,7):'B',(7,4):'B',(7,5):'C',(5,8):'C',(8,10):'C',(10,7):'C',(11,9):'D',(9,12):'D',(12,13):'D',(13,11):'D'}
-# i_p_r = {4:[('r1','A'),('r2','B')],13:[('r3','D')]}
-# simula_drones(my_g, i_p_r, True, None, e_circles)
-
-
 # i_p_r = {4:['r1','r2'],13:['r3']}
-#simula_drones(my_g, i_p_r, True, None)
+
+# e_circles = {(1,3):'A',(3,4):'A',(4,2):'A',(2,1):'A',(4,6):'B',(6,9):'B',(9,7):'B',(7,4):'B',(7,5):'C',(5,8):'C',(8,10):'C',(10,7):'C',(11,9):'D',(9,12):'D',(12,13):'D',(13,11):'D'}
+# simula_drones(my_g, i_p_r, True, None,{'r1':'A','r2':'B','r3':'D'},e_circles)
+
+
+
+# simula_drones(my_g, i_p_r, True, None)
